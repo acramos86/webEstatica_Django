336d00e7b3825f323bdd218937ce86d9138e25e0
Alex Ramos <acramos86@gmail.com>
2020-11-27 17:06:35 -0500

    Dejando proyecto funcionando de forma local

--- proyectoDjango/settings.py
+++ proyectoDjango/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '40u$^y@v63m)$g7m_)=pwtdryk_cmwx3r)rrv_7@*lafqyk@0#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
